50068171747427ae2361d28cf973b8824f14b969
selfmaden <34777425+selfmaden@users.noreply.github.com>
2018-01-05 11:19:39 +0300

    Update Search.java

--- primepaths/Search.java
+++ primepaths/Search.java
@@ -1,14 +1,28 @@
 package paths;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.LinkedList;
-//import java.util.Scanner;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
 
 
 public class Search {
 
     public static int counter = 0; 
+    int r=0;
+
     
-		    public void depthFirstSearch(Graph graph, LinkedList<Integer> visited, int end) {
+                static String excelFileName = "C:/temp/demo.xls";//name of excel file
+
+		static String sheetName = "Sheet1";//name of sheet
+    		static HSSFWorkbook wb = new HSSFWorkbook();
+		static HSSFSheet sheet = wb.createSheet(sheetName) ;
+    
+		    public void depthFirstSearch(Graph graph, LinkedList<Integer> visited, int end) throws IOException {
 		        LinkedList<Integer> nodes = graph.adjacentNodes(visited.getLast());
 		        // examine adjacent nodes
 		        
@@ -37,21 +51,32 @@ public class Search {
 		    }
 		    
 		    
-		    private void printPath(LinkedList<Integer> visited) {
+		    private void printPath(LinkedList<Integer> visited) throws IOException {
                         
-                        
-					
+                      
+                      HSSFRow row = sheet.createRow(r);
+                            int c=0;
 		    	for (Integer node : visited) {
-		
+                            			
+                                                HSSFCell cell = row.createCell(c);
+				
+                                                cell.setCellValue( node);
 						System.out.print(node);
-						System.out.print(" ");		            
+						System.out.print(" ");	
+                                                c++;
 		        }
-
+                        
+                        r++;
 		        System.out.println();
+                        
+                FileOutputStream fileOut = new FileOutputStream(excelFileName);
+		
+		//write this workbook to an Outputstream.
+		wb.write(fileOut);
+		fileOut.flush();
+		fileOut.close();
 		        
 		    }
-		    
 
-    
     }
 	
